29433f433f3f1a6e807877df782beb683b977b4a
Berchez <walterfelipeberchez@outlook.com>
2022-10-05 13:07:55 -0300

    2 commit

--- hooks-2-react/src/templates/Home/index.jsx
+++ hooks-2-react/src/templates/Home/index.jsx
@@ -1,4 +1,9 @@
+import { useCounterContext } from '../../contexts/CounterContext';
+
 export const Home = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [state, dispatch] = useCounterContext();
+
   return (
     <div>
       <h1>Oi</h1>
