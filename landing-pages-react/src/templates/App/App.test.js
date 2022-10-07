5ada96791e9e59085e96671f22b826b949a4be6f
Berchez <walterfelipeberchez@outlook.com>
2022-10-07 13:51:50 -0300

    styled-components

--- landing-pages-react/src/templates/App/App.test.js
+++ landing-pages-react/src/templates/App/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import Home from './App';
+import Home from '.';
+import { renderTheme } from '../../styles/render-theme';
+import { screen } from '@testing-library/react';
 
 test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // eslint-disable-next-line no-unused-vars
+  const { debug } = renderTheme(<Home />);
+  const headingContainer = screen.getByRole('heading', {
+    name: 'Hello',
+  }).parentElement;
+  expect(headingContainer).toMatchSnapshot();
+  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
