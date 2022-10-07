8e16aab6cfa0b0da3d9d322d525268621008d0f6
Berchez <walterfelipeberchez@outlook.com>
2022-10-07 12:55:55 -0300

    styled-components

--- landing-pages-react/src/templates/App/index.jsx
+++ landing-pages-react/src/templates/App/index.jsx
@@ -1,7 +1,10 @@
+import * as Styled from './styles';
 function Home() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Styled.Wrapper background="blue">
+        <h1>Hello</h1>
+      </Styled.Wrapper>
     </div>
   );
 }
