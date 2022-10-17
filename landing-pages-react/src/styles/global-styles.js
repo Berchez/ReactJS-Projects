bd7ac3c31140b5007f88550b5169f2a49e3ef639
Berchez <walterfelipeberchez@outlook.com>
2022-10-17 10:10:03 -0300

    GridContent

--- landing-pages-react/src/styles/global-styles.js
+++ landing-pages-react/src/styles/global-styles.js
@@ -19,8 +19,21 @@ export const GlobalStyles = createGlobalStyle`
     left: 100%
   }
 
-  h1{
-    font-family: ${({ theme }) => theme.font.family.secondary}
+  h1, h2, h3, h4, h5, h6{
+    font-family: ${({ theme }) => theme.font.family.secondary};
+    margin: ${({ theme }) => theme.spacings.large} 0;
   }
 
+  p{
+    margin: ${({ theme }) => theme.spacings.medium} 0;
+  }
+
+  ul, ol{
+    margin: ${({ theme }) => theme.spacings.medium};
+    padding: ${({ theme }) => theme.spacings.medium};
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.secondaryColor}
+  }
 `;
