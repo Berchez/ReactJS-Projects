ef17d28b57c19aa7c42c0588240f6850c50622bf
Berchez <walterfelipeberchez@outlook.com>
2022-10-17 11:30:38 -0300

    GridText

---
+++ landing-pages-react/src/components/GridText/mock.js
@@ -0,0 +1,23 @@
+export default {
+  background: false,
+  title: 'My grid',
+  description: 'Anim deserunt tempor est eu voluptate.',
+  grid: [
+    {
+      title: 'Title 1',
+      description: `Sit duis non incididunt incididunt voluptate ad sit officia. Voluptate ex ad cillum deserunt labore aute occaecat sint irure amet. Sit velit deserunt dolore ea sunt in proident sit anim irure deserunt. Fugiat Lorem sunt velit qui commodo ut dolore qui. Anim incididunt laboris magna amet. Est veniam velit tempor id.`,
+    },
+    {
+      title: 'Title 2',
+      description: `Sit duis non incididunt incididunt voluptate ad sit officia. Voluptate ex ad cillum deserunt labore aute occaecat sint irure amet. Sit velit deserunt dolore ea sunt in proident sit anim irure deserunt. Fugiat Lorem sunt velit qui commodo ut dolore qui. Anim incididunt laboris magna amet. Est veniam velit tempor id.`,
+    },
+    {
+      title: 'Title 3',
+      description: `Sit duis non incididunt incididunt voluptate ad sit officia. Voluptate ex ad cillum deserunt labore aute occaecat sint irure amet. Sit velit deserunt dolore ea sunt in proident sit anim irure deserunt. Fugiat Lorem sunt velit qui commodo ut dolore qui. Anim incididunt laboris magna amet. Est veniam velit tempor id.`,
+    },
+    {
+      title: 'Title 4',
+      description: `Sit duis non incididunt incididunt voluptate ad sit officia. Voluptate ex ad cillum deserunt labore aute occaecat sint irure amet. Sit velit deserunt dolore ea sunt in proident sit anim irure deserunt. Fugiat Lorem sunt velit qui commodo ut dolore qui. Anim incididunt laboris magna amet. Est veniam velit tempor id.`,
+    },
+  ],
+};
