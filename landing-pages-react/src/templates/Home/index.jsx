91b6d57584c42a0f8cf660bfd1fd51f2ba9abe3f
Berchez <walterfelipeberchez@outlook.com>
2022-10-26 15:37:16 -0300

    adding components to page

--- landing-pages-react/src/templates/Home/index.jsx
+++ landing-pages-react/src/templates/Home/index.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
-import { Base } from '../Base';
-import { mockBase } from '../Base/mock';
 import { mapData } from '../../api/map-data';
 import { PageNotFound } from '../PageNotFound';
 import { Loading } from '../Loading';
+import { GridTwoColumns } from '../../components/GridTwoColumns';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
+import { Base } from '../Base';
 
 function Home() {
   const [data, setData] = useState([]);
@@ -12,20 +15,22 @@ function Home() {
     const load = async () => {
       try {
         const data = await fetch(
-          'http://localhost:1337/api/pages/?slug=olha-so-a-minha-pagina',
+          'http://localhost:1337/api/pages/?slug=olha-so-a-minha-pagina&populate=deep',
         );
 
         const json = await data.json();
         const { attributes } = json.data[0];
         const pageData = mapData([attributes]);
 
+        //Atraso proposital com finalidade apenas de mostrar a tela de loading
         await new Promise((r) => {
           return setTimeout(() => {
             setData(() => pageData[0]);
             r();
           }, 2000);
         });
-      } catch {
+      } catch (e) {
+        console.log(e);
         setData(undefined);
       }
     };
@@ -40,7 +45,40 @@ function Home() {
     return <Loading />;
   }
 
-  return <Base {...mockBase} />;
+  const { menu, sections, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+  console.log(data);
+
+  return (
+    <Base
+      links={links}
+      footerHtml={footerHtml}
+      logoData={{ text, link, srcImg }}
+    >
+      {sections.map((section, index) => {
+        const { component } = section;
+        const key = `${slug}-${index}`;
+
+        console.log('a', component);
+
+        if (component === 'section.section-two-columns') {
+          return <GridTwoColumns key={key} {...section} />;
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />;
+        }
+      })}
+    </Base>
+  );
 }
 
 export default Home;
