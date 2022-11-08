8b1aef154920b20ca729163a11ed9f77faed7ce1
Berchez <walterfelipeberchez@outlook.com>
2022-11-08 11:10:36 -0300

    dynamics routes

---
+++ landing-pages-next/src/pages/[slug].jsx
@@ -0,0 +1,27 @@
+import P from 'prop-types';
+import {loadPages} from '../api/load-pages';
+import Home from '../templates/Home';
+
+export default function Page({data}) {
+  return <Home data={data} />
+}
+
+Page.propType = {
+  data: P.object,
+}
+
+export const getStaticPaths = async () => {
+  const paths = (await loadPages()).map((page) => {
+    return {
+      params: {
+        slug: page.slug,
+      }
+    }
+  });
+
+  return {
+    paths,
+    fallback: false,
+  }
+
+}
