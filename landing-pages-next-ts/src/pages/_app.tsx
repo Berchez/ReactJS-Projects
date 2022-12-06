c5c22fe5864723176eb5524d7a77a0a08950b874
Berchez <walterfelipeberchez@outlook.com>
2022-12-06 11:44:05 -0300

    pages folder

--- landing-pages-next-ts/src/pages/_app.tsx
+++ landing-pages-next-ts/src/pages/_app.tsx
@@ -1,11 +1,10 @@
-import P from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 import { AppProps } from 'next/app';
 
 import { GlobalStyles } from '../styles/global-styles';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
@@ -15,5 +14,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
-
-MyApp.propTypes = AppProps;
