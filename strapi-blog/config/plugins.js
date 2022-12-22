63dd7fb9f758302ca25ec5bee4e871e5d3f2bed1
Berchez <walterfelipeberchez@outlook.com>
2022-12-22 12:49:39 -0300

    category, tag, author

--- strapi-blog/config/plugins.js
+++ strapi-blog/config/plugins.js
@@ -4,8 +4,8 @@ module.exports = ({ env }) => ({
       provider: 'cloudinary',
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDNARY_KEY'),
-        api_secret: env('CLOUDNARY_SECRET'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
         upload: {},
