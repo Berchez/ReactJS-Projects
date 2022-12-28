5a552203895ee9a715a63d0c53877e3822d176a6
Berchez <walterfelipeberchez@outlook.com>
2022-12-28 17:03:55 -0300

    HtmlContent styled-components

--- blog-next/src/components/HtmlContent/stories.tsx
+++ blog-next/src/components/HtmlContent/stories.tsx
@@ -1,20 +1,18 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { HtmlContent, HtmlContentProps } from '.';
+import mock from './mock';
 
 export default {
   title: 'HtmlContent',
   component: HtmlContent,
   args: {
-    html: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Magni expedita exercitationem earum, amet at quasi obcaecati!
-              Doloribus dignissimos ducimus ea molestias minima tenetur,
-              tempore id facilis? Magni eaque eius voluptatum!`,
+    html: mock,
   },
 } as Meta;
 
 export const Template: Story<HtmlContentProps> = (args) => {
   return (
-    <div>
+    <div style={{ maxWidth: '800px', margin: '0 auto' }}>
       <HtmlContent {...args} />
     </div>
   );
