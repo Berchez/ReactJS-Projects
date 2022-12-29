781ebd5649186371f92b684c0ea9c6d0697f648a
Berchez <walterfelipeberchez@outlook.com>
2022-12-29 14:09:29 -0300

    Header

---
+++ blog-next/src/components/Header/mock.ts
@@ -0,0 +1,11 @@
+import { HeaderProps } from '.';
+
+export default {
+  blogName: 'Walter Berchez',
+  blogDescription: `Commodo aliqua veniam dolor labore elit proident. Est occaecat et ut do labore amet nulla nisi
+    enim exercitation consequat velit. Qui nisi aliquip et esse in nisi tempor aute. Dolor
+    reprehenderit amet cillum commodo qui veniam fugiat commodo ut magna aliquip do dolor labore.
+    Cillum qui labore quis culpa.`,
+  logo: '/assets/images/me.png',
+  showText: true,
+} as HeaderProps;
