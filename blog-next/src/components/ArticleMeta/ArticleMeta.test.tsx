7206708d85f4954d3190bbc4b7cbe73d164265a0
Berchez <walterfelipeberchez@outlook.com>
2023-01-02 08:44:35 -0300

    ArticleMeta

---
+++ blog-next/src/components/ArticleMeta/ArticleMeta.test.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { ArticleMeta, ArticleMetaProps } from '.';
+
+import mock from './mock';
+
+const props: ArticleMetaProps = mock;
+
+describe('<ArticleMeta />', () => {
+  it('should render author and category links ', () => {
+    renderTheme(<ArticleMeta {...props} />);
+
+    expect(
+      screen.getByRole('link', { name: 'Walter Berchez' }),
+    ).toHaveAttribute('href', { name: 'Walter Berchez' });
+
+    expect(screen.getByRole('link', { name: 'JS' })).toHaveAttribute(
+      'href',
+      '/category/javascript',
+    );
+  });
+
+  it('should format date', () => {
+    renderTheme(<ArticleMeta {...props} />);
+    expect(screen.getByText('29 de dez. de 2022')).toHaveAttribute(
+      'datetime',
+      props.createdAt,
+    );
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<ArticleMeta {...props} />);
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ blog-next/src/components/ArticleMeta/mock.ts
@@ -0,0 +1,21 @@
+import { ArticleMetaProps } from '.';
+export default {
+  createdAt: '2022-12-29T15:00:00.246+00:00',
+  author: {
+    id: '12312',
+    slug: 'walter-berchez',
+    displayName: 'Walter Berchez',
+  },
+  categories: [
+    {
+      id: '12sdf',
+      displayName: 'Tech',
+      slug: 'tech',
+    },
+    {
+      id: 'afasg',
+      displayName: 'JS',
+      slug: 'javascript',
+    },
+  ],
+} as ArticleMetaProps;
