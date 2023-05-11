c51de4816e5547e8fddf2877d1ea5223adb21f00
Berchez <walterfelipeberchez@outlook.com>
2023-05-11 00:06:19 -0300

    feat(front): the title and description

--- reservate-next-13/app/page.tsx
+++ reservate-next-13/app/page.tsx
@@ -29,6 +29,51 @@ export default function Home() {
             </div>
           </div>
           {/* HEADER */}
+          {/* DESCRIPTION PORTION */}
+          <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+            <div className="bg-white w-[70%] rounded p-3 shadow">
+              {/* RESTAURANT NAVBAR */}
+              <nav className="flex text-reg border-b pb-2">
+                <a href="" className="mr-7">
+                  Overview
+                </a>
+                <a href="" className="mr-7">
+                  Menu
+                </a>
+              </nav>
+              {/* RESTAURANT NAVBAR */}
+              {/* TITLE */}
+              <div className="mt-4 border-b pb-6">
+                <h1 className="font-bold text-6xl">Milesstone Grill</h1>
+              </div>
+              {/* TITLE */}
+              {/* RATING */}
+              <div className="flex items-end">
+                <div className="ratings mt-2 flex items-center">
+                  <p>*****</p>
+                  <p className="text-reg ml-3">4.9</p>
+                </div>
+                <div>
+                  <p className="text-reg ml-4">600 Reviews</p>
+                </div>
+              </div>
+              {/* RATING */}
+              {/* DESCRIPTION */}
+              <div className="mt-4">
+                <p className="text-lg font-light">
+                  3 Course Dinner for two, Short rib wontons, smoky chili oil,
+                  garlic crunch (NF, DF) Dungeness crab spring onion pancake
+                  (NF, DF) Halibut, spring onion oil, tomato nuoc cham, sugar
+                  snap pease (NF, DF, GF) Grass-fed strip loin steak, spring
+                  radish, "A1" (NF, DF, GF) Coconut cheesecake, rhubarb, yuzu
+                  caramel (NF, DF, GF) Mochi cake, sour cherries, chocolate
+                  pomegranate sauce (NF, GF)
+                </p>
+              </div>
+              {/* DESCRIPTION */}
+            </div>
+          </div>
+          {/* DESCRIPTION PORTION */}
         </main>
       </main>
     </main>
