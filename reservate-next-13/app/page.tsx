2b2611e09b8b490b5d6d8d4d10437c65bab9993a
Berchez <walterfelipeberchez@outlook.com>
2023-05-15 23:24:41 -0300

    feat(front): programmatic navigation

--- reservate-next-13/app/page.tsx
+++ reservate-next-13/app/page.tsx
@@ -1,6 +1,12 @@
+'use client';
+
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+  const [location, setLocation] = useState('');
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-ayto bg-white">
@@ -34,8 +40,16 @@ export default function Home() {
                   className="bg-white rounded text-lg mr-3 p-2 w-[450px]"
                   type="text"
                   placeholder="State, city or town"
+                  value={location}
+                  onChange={(e) => setLocation(e.target.value)}
                 />
-                <button className="rounded bg-red-600 px-9 py-2 text-white">
+                <button
+                  className="rounded bg-red-600 px-9 py-2 text-white"
+                  onClick={() => {
+                    if (location === 'banana') return;
+                    router.push('/search');
+                  }}
+                >
                   Let's go
                 </button>
               </div>
