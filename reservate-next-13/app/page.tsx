231aaba09933bc6b8a4537b103d88a24b88ebfa9
Berchez <walterfelipeberchez@outlook.com>
2023-05-23 02:53:29 -0300

    feat(front): creating type

--- reservate-next-13/app/page.tsx
+++ reservate-next-13/app/page.tsx
@@ -1,22 +1,43 @@
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
+import { Cuisine, Location, PRICE } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const fetchRestaurants = async () => {
-  const restaurants = await prisma.restaurant.findMany();
-  return restaurants;
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
 }
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+    },
+  });
+  return restaurants;
+};
 
 export default async function Home() {
   const restaurants = await fetchRestaurants();
-  console.log({restaurants});
+  console.log({ restaurants });
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap">
-        <RestaurantCard />
+        {restaurants.map((restaurant, index) => (
+          <RestaurantCard key={index} restaurant={restaurant} />
+        ))}
       </div>
     </main>
   );
