e6905e52af0f1bf044ce62d5a5d3c6a81a475399
Berchez <walterfelipeberchez@outlook.com>
2023-05-23 01:08:27 -0300

    feat(front): fetching data in a server component

--- reservate-next-13/app/page.tsx
+++ reservate-next-13/app/page.tsx
@@ -1,15 +1,23 @@
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async () => {
+  const restaurants = await prisma.restaurant.findMany();
+  return restaurants;
+}
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+  console.log({restaurants});
   return (
     <main>
       <Header />
-      {/* CARDS */}
       <div className="py-3 px-36 mt-10 flex flex-wrap">
         <RestaurantCard />
       </div>
-      {/* CARDS */}
     </main>
   );
 }
